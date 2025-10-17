8a65336c786585b72c58e713453073b49e7e1904
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-18 01:38:34 +0500

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@
 import './App.css'
 import ContactCard from './components/ContactCard'
 
+
 function App() {
   
 
   return (
     <>
-      <ContactCard/>
+      <ContactCard 
+      id={1} 
+      name={'Fahad'} 
+      dob={'1/25/1998'} 
+      grossSalary={50000}/>
     </>
   )
 }
